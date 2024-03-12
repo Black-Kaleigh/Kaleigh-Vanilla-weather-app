03b1b6d26ae5825f73edd453f9aedae0d13b76dc
Black-Kaleigh <BLACKTAMARAKALEIGH@GMAIL.COM>
2024-03-12 16:45:09 +0200

    updated css

--- src/index.js
+++ src/index.js
@@ -64,8 +64,8 @@ function formatDay(timestamp) {
 
 function getForecast(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayForecast(response) {
